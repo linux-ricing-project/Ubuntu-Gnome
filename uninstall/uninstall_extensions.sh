79cade53c6b2fd35571859d060e0802090951f7c
Frank Junior <frankcbjunior@gmail.com>
2020-04-02 16:42:56 -0300

    created install/uninstall extensions

---
+++ uninstall/uninstall_extensions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# ============================================
+# Função de debug
+# ============================================
+log(){
+  echo "[LOG] $*"
+}
+
+# desabilita todas as extensões
+log "Desabilitando todas as extensões"
+gsettings set org.gnome.shell enabled-extensions "[]"
+
+# se existir a pasta de extensions, delete o conteúdo dela
+log "Removendo o diretório de extensões"
+extensions_path="$HOME/.local/share/gnome-shell/extensions"
+test -d "$extensions_path" && rm -rf "$extensions_path"
+
+log "Removendo todos os schemas"
+local_schema_path="$HOME/.local/share/glib-2.0/schemas/"
+if [ -d "$local_schema_path" ];then
+  for schemas in $(find $local_schema_path -type l);do
+  rm -rf "$schemas"
+  done
+fi
